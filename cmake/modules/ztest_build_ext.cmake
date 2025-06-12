98919a513e3e66f6105f7967364bd6fe86bbd4be
htringuyen <tringuyenh75@gmail.com>
2025-06-12 22:17:44 +0700

    Add new cmake module for testing

---
+++ cmake/modules/ztest_build_ext.cmake
@@ -0,0 +1,26 @@
+include_guard(GLOBAL)
+
+if (${BOARD} STREQUAL "unit_testing_ext/unit_testing")
+
+    #set(EXTRA_ZEPHYR_MODULES /home/htring/embc/ztest-build-ext)
+
+    list(APPEND EXTRA_ZEPHYR_MODULES /home/htring/embc/ztest-build-ext)
+
+    message(STATUS "Loading ztest-build-ext")
+    include(extensions)
+    include(west)
+    include(yaml)
+    include(root)
+    include(zephyr_module)
+
+    if (NOT DEFINED ZEPHYR_ZTEST_BUILD_EXT_MODULE_DIR)
+        message(FATAL_ERROR "Ztest build extension module is required but not found.")
+    endif()
+
+    add_executable(app "")
+
+    add_subdirectory(${ZEPHYR_ZTEST_BUILD_EXT_MODULE_DIR} ${CMAKE_BINARY_DIR}/ext)
+else()
+    zephyr_package_message(NOTICE "Loading Zephyr default modules (${location}).")
+    include(zephyr_default NO_POLICY_SCOPE)
+endif()
