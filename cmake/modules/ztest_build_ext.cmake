ecfa668ee61690364f39c4945049e4cdb0681d0c
htringuyen <tringuyenh75@gmail.com>
2025-06-13 14:20:30 +0700

    Update module loading

--- cmake/modules/ztest_build_ext.cmake
+++ cmake/modules/ztest_build_ext.cmake
@@ -2,7 +2,9 @@ include_guard(GLOBAL)
 
 if (${BOARD} STREQUAL "unit_testing_ext/unit_testing")
 
-    #set(EXTRA_ZEPHYR_MODULES /home/htring/embc/ztest-build-ext)
+    if (NOT DEFINED UT_MODULE_DIR)
+        message(FATAL_ERROR "Ztest build extension module is required but not found.")
+    endif()
 
     list(APPEND EXTRA_ZEPHYR_MODULES ${UT_MODULE_DIR})
 
@@ -13,13 +15,9 @@ if (${BOARD} STREQUAL "unit_testing_ext/unit_testing")
     include(root)
     include(zephyr_module)
 
-    if (NOT DEFINED ZEPHYR_ZTEST_BUILD_EXT_MODULE_DIR)
-        message(FATAL_ERROR "Ztest build extension module is required but not found.")
-    endif()
-
     add_executable(app "")
 
-    add_subdirectory(${ZEPHYR_ZTEST_BUILD_EXT_MODULE_DIR} ${CMAKE_BINARY_DIR}/ext)
+    add_subdirectory(${UT_MODULE_DIR} ${CMAKE_BINARY_DIR}/ext)
 else()
     zephyr_package_message(NOTICE "Loading Zephyr default modules (${location}).")
     include(zephyr_default NO_POLICY_SCOPE)
